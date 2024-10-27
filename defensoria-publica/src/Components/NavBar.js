e5b696eb3f7116797b6fb2aa3170f000ebfb9334
unknown <tiagoalvesfernandes81@gmail.com>
2024-10-27 20:44:03 -0300

    add coments in functions

---
+++ defensoria-publica/src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// Os css`s de componentes estarão na pasta Style/Components/NomeDaPagina.css
+// Para importar basta digitar import "./Style/Components/NomeDaPagina.css"
+
+function NavBar() {
+  return (
+    // Dentro deste Return, vão os codigos HTML, que serão lidos e interpretados pelo React.
+    // Neste componente, devem ir apenas os codigos referentes á construção da barra de navegação.
+    // A barra que normalmente vemos no topo das paginas.
+
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
--- defensoria-publica/src/Pages/Socials.js
+++ defensoria-publica/src/Pages/Socials.js
@@ -1,7 +1,13 @@
 import React from 'react'
+// Os css`s de pagina estarão na pasta Style/Pages/NomeDaPagina.css
+// Para importar basta digitar import "./Style/Pages/NomeDaPagina.css"
 
 function Socials() {
   return (
+    // Dentro deste Return, vão os codigos HTML, que serão lidos e interpretados pelo React.
+    // Nesta Pagina, devem ser chamados os componentes que compõem a pagina Home.
+    // Um exemplo será a NavBar, que será chamada em todas as paginas, ja que toda pagina terá o elemento de navegação
+
     <div>Socials</div>
   )
 }
