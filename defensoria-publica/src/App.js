fcd39b7d53e794bfd9994711d49719f9ec02063e
TIAGO ALVES FERNANDES <24101255@aluno.idp.edu.br>
2024-11-08 11:32:01 -0300

    chore: add navbar first codes

--- defensoria-publica/src/App.js
+++ defensoria-publica/src/App.js
@@ -1,9 +1,11 @@
 import Carrousel from './Components/Carrousel';
+import NavBar from './Components/NavBar';
 import './Style/App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Carrousel />
     </div>
   );
--- defensoria-publica/src/Components/Carrousel.js
+++ defensoria-publica/src/Components/Carrousel.js
@@ -1,32 +1,66 @@
-import React from 'react'
-import { personas } from '../Utils/mocks/persona'
-// Os css`s de componentes estarão na pasta Style/Components/NomeDaPagina.css
-// Para importar basta digitar import "./Style/Components/NomeDaPagina.css"
-// Neste componente, devem ir apenas os codigos referentes á construção do carrosel.  
-
-
-// Dica de ouro : Em react é possivel usar funções javascript para realizar ações mais complexas.
-// Normalmente neste espaço abaixo
-
-
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import { personas } from '../Utils/mocks/persona';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import "../Style/Components/carrousel.css"; // Certifique-se de criar e estilizar este arquivo CSS
 
 function Carrousel() {
-  // Dentro deste Return, vão os codigos HTML, que serão lidos e interpretados pelo React.
-  // O array personas está importado e pode ser ultilizado, basta percorrer o array usando uma função chamada de .map()
-  // O `map` pode ser usado diretamente no `return` para gerar o conteúdo.
+  const [sliderRef, setSliderRef] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentPersona, setCurrentPersona] = useState(null);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (current, next) => setCurrentPersona(personas[next]),
+  };
+
+  const handleNext = () => {
+    if (sliderRef) {
+      sliderRef.slickNext();
+    }
+  };
+
+  const handlePopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
     <div className="carousel">
       <h2>Carrousel</h2>
-      {personas.map((elementoDoArray, index) => {
-        const { name, personaHistory, srcImage } = elementoDoArray;
-        return (
-          <div key={index} className="carousel-item">
-            <h3>{name}</h3>
-            <p>{personaHistory}</p>
-            <img src={srcImage} alt={`${name} profile`} />
+      <Slider ref={setSliderRef} {...settings}>
+        {personas.map((elementoDoArray, index) => {
+          const { name, personaHistory, srcImage } = elementoDoArray;
+          return (
+            <div key={index} className="carousel-item">
+              <h3>{name}</h3>
+              <p>{personaHistory}</p>
+              <img src={srcImage} alt={`${name} profile`} />
+            </div>
+          );
+        })}
+      </Slider>
+      <div className="carousel-buttons">
+        <button onClick={handleNext}>Próximo</button>
+        <button onClick={handlePopup}>Abrir Pop-up</button>
+      </div>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <h3>{currentPersona?.name}</h3>
+            <p>{currentPersona?.personaHistory}</p>
+            <button onClick={closePopup}>Fechar</button>
           </div>
-        );
-      })}
+        </div>
+      )}
     </div>
   );
 }
