186838624e1f2b9b3445a0a249920b49ae735418
Allan <allansamuel512@gmail.com>
2024-11-09 20:19:44 -0300

    personas alterada

--- defensoria-publica/src/Utils/mocks/persona.js
+++ defensoria-publica/src/Utils/mocks/persona.js
@@ -2,13 +2,28 @@
 
 export const personas = [
 {
-    name: "Maria",
+    name: "Sandra",
     srcImage: "link pra imagem",
-    personaHistory: "Historia da pessoa e texto pra ser ultilizado"
+    personaHistory: "Perfil: Sandra é uma mulher de 33 anos, negra, mãe solteira, e trabalha como auxiliar de limpeza em uma grande cidade. Ela mora em uma área periférica e enfrenta dificuldades financeiras para sustentar seus dois filhos pequenos. Sandra sempre teve uma vida difícil, enfrentando discriminação racial e de gênero, além de assédio moral no trabalho. Problema: Ela busca a Defensoria Pública para garantir o acesso ao direito à moradia digna após receber uma ordem de despejo. Sandra também procura apoio para conseguir uma vaga em uma creche pública para seus filhos, mas enfrenta dificuldades devido à falta de vagas e o processo burocrático. Ela sente que o sistema é mais difícil para mulheres negras e periféricas, e acredita estar sendo discriminada. Necessidade: Garantir moradia e inclusão social adequada, com acesso à educação para seus filhos e a igualdade de tratamento em serviços públicos."
 },
 {
-    name: "Mario",
+    name: "Lucas",
     srcImage: "link pra imagem",
-    personaHistory: "Historia da pessoa e texto pra ser ultilizado"
-}
+    personaHistory: "Perfil: Lucas é um jovem de 22 anos desempregado, morador de uma comunidade carente. Ele foi preso em flagrante durante uma batida policial em sua comunidade, acusado de portar drogas para o tráfico, embora ele afirme que estava apenas no local errado na hora errada. Lucas não tem antecedentes criminais, mas está enfrentando dificuldades para provar sua inocência. Problema: Lucas e sua família não têm condições de contratar um advogado particular, por isso, ele busca a Defensoria Pública para se defender das acusações. Sua mãe está desesperada, temendo que ele seja condenado por algo que não fez. Necessidade: Defesa criminal para garantir que Lucas tenha um julgamento justo e consiga provar sua inocência, evitando uma condenação injusta."
+},
+{ 
+    name: "Júlia",
+    scrImage: "link pra imagem",
+    personaHistory: "Perfil: Júlia é uma estudante do ensino médio de 16 anos que sofreu um acidente que a deixou paraplégica e agora tem que usar cadeira de rodas para poder se movimentar. Além disso, a instituição de ensino de Júlia não possui adaptações para sua acessibilidade, o que dificulta o seu ensino e a impossibilita de participar ativamente das aulas. Problema: Júlia procura a defensoria pública para garantir seus direitos de acessibilidade e garantir uma ação judicial contra a escola caso ela continue a negar seus direitos a fim de forçar a instituição a aplicar as devidas alterações. Não só isso, ela deseja também buscar programas de assistência ou recursos que melhorem a infraestrutura do instituto. Necessidade: Garantia dos direitos de acessibilidade e busca de programas de assistência à família."
+},
+{
+    name: "Anderson",
+    scrImage: "link pra imagem",
+    personaHistory: "Perfil: Anderson tem 42 anos, é um ex-funcionário de uma grande empresa de mineração que, ao longo de sua carreira, foi exposto a condições de trabalho insalubres, incluindo a inalação contínua de poeira e produtos químicos tóxicos. Ele desenvolveu uma doença pulmonar grave, que o impossibilita de continuar trabalhando. Problema: Quando tentou solicitar sua aposentadoria por invalidez junto ao INSS, seu pedido foi negado, com o argumento de que ele ainda teria condições de trabalhar em outras funções. Ao mesmo tempo, sua antiga empresa se nega a reconhecer a responsabilidade pelas condições de trabalho, não oferecendo compensação adequada pelos danos sofridos. Necessidade: Anderson busca a Defensoria Pública para mover uma ação trabalhista contra a empresa por danos à sua saúde e uma ação contra o INSS para reverter a decisão e garantir sua aposentadoria por invalidez, além de assistência médica integral para tratar sua condição."
+},
+{
+    name: "Rodrigo",
+    scrImage: "link pra imagem",
+    personaHistory: "Perfil: Rodrigo tem 19 anos e busca a Defensoria Pública para mover uma ação trabalhista contra a empresa por danos à sua saúde e uma ação contra o INSS para reverter a decisão e garantir sua aposentadoria por invalidez, além de assistência médica integral para tratar sua condição. Problema: O motorista envolvido no acidente fugiu sem prestar socorro, e o aplicativo de entregas se eximiu de responsabilidade, não oferecendo nenhum tipo de assistência ou indenização. Sem seguro e sem poder trabalhar, Rodrigo está em uma situação financeira muito complicada, com sua família dependendo de sua renda. Necessidade: Ele procura :a Defensoria Pública para entrar com uma ação contra o aplicativo por negligência trabalhista e buscar uma compensação pelos danos sofridos, além de responsabilizar o motorista do acidente."
+},
 ]
