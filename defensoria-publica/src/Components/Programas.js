53264d5dd1cffd6b47e19ad4eccba8aae8494eea
Samuel Abrão <163413922+samuka7abr@users.noreply.github.com>
2024-11-22 18:53:27 -0300

    Update Programa-Style

--- defensoria-publica/src/Components/Programas.js
+++ defensoria-publica/src/Components/Programas.js
@@ -4,19 +4,17 @@ import './../Style/Components/Programas.css';
 function Programas() {
   return (
     <div className="programas-container">
-      {programas.map((programa) => (
-        <div key={programa.id} className="programa-card">
-          <a href={programa.link} target="_blank" rel="noopener noreferrer">
-            <img
-              src={programa.srcImage}
-              alt={programa.name}
-              className="programa-image"
-            />
-            <h3 className="programa-name">{programa.name}</h3>
-          </a>
-        </div>
-      ))}
+  {programas.map((programa) => (
+    <div key={programa.id} className="programa-item">
+      <img
+        src={programa.srcImage}
+        alt={programa.name}
+        className="programa-image"
+      />
+      <div className="programa-name">{programa.name}</div>
     </div>
+  ))}
+</div>
   );
 }
 
